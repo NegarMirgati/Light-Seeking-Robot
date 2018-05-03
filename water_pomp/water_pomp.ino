243b89b07af303ab0426edfe6d0619da75f80e24
Negar Mirgati <negar.nbj96@yahoo.com>
2018-05-03 16:25:40 +0430

    Add water pomp initial code

---
+++ water_pomp/water_pomp.ino
@@ -0,0 +1,19 @@
+int motorPin = 3;
+ 
+void setup() 
+{ 
+  pinMode(motorPin, OUTPUT);
+  Serial.begin(9600);
+  while (! Serial);
+  Serial.println("Speed 0 to 255");
+} 
+ 
+ 
+void loop() 
+{ 
+
+      analogWrite(motorPin, 255);
+
+
+} 
+
