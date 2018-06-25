f36280b1f82f29fca2d0278458d6f447bfbb8426
Negar Mirgati <negar.nbj96@yahoo.com>
2018-06-25 15:17:04 +0430

    Add 2 light sensors code and 2 servos

--- new_move/new_move.ino
+++ new_move/new_move.ino
@@ -29,24 +29,21 @@ Servo Servo2;
 int servoPin1 = 9; 
 int ServoPin2 = 10;
 
-
-
 const int RightMotorIN3 = 5;    // This pin is used to enable or disable the Right motor. Connected to the base of an NPN transistor.
 const int RightMotorIN4 = 4;    // This pin is used to enable or disable the Right motor. Connected to the base of an NPN transistor.
 const int LeftMotorIN1 = 7;     // This pin is used to enable or disable the Left motor. Connected to the base of an NPN transistor.
 const int LeftMotorIN2 = 6;     // This pin is used to enable or disable the Left motor. Connected to the base of an NPN transistor.
 
-const int FrontSensor = 11;
-const int BackSensor = 8 ;
+const int BackSensor = 11;
+const int FrontSensor = 8 ;
 
 
-const int ENA = 10;
-const int ENB = 3;
-
-
+//const int ENA = 10;
+//const int ENB = 3;
 
 // Variable definitions
 int degreeMax;
+bool max_in_front;
 int maxLX;
 int SensorDifference;   // This value is used to determine the difference between the Left and Right
 
@@ -99,20 +96,80 @@ int SensorDifference;   // This value is used to determine the difference betwee
     Serial.println("Backward");                                      // This prints Left when the robot would actually turn Left.
  }
 
+void calc_degree(){
+   Serial.println(" Calc_degree called");
+   Servo1.write(0); 
+   Servo2.write(0);
+   maxLX = 0 ; 
+   degreeMax = 0;
+   //for(int cnt = 0 ; cnt <= 1; cnt++){
+   for(int j = 0; j <= 6; j++){
+      
+      /*(cnt == 0) ? */
+      Servo1.write(j*30); 
+      Servo2.write(j*30);    
+      delay(100);
+      
+      digitalWrite(FrontSensor, LOW);       // sets the digital_out_11 on -> front
+      digitalWrite(BackSensor, HIGH);       // sets the digital_out_8 off -> back
+      uint16_t luxb = lightMeter.readLightLevel();
+      
+      delay(200); 
+         
+      digitalWrite(FrontSensor, HIGH);       // sets the digital_out_11 on -> front
+      digitalWrite(BackSensor, LOW);       // sets the digital_out_8 off -> back
+      uint16_t luxf = lightMeter.readLightLevel();
+      
+      /*Serial.print("Light F: ");
+      Serial.print(luxf);
+      Serial.println(" lx");
+      Serial.print("Light B: ");
+      Serial.print(luxb);
+      Serial.println(" lx");
+      Serial.println("the other sensor");*/
+      if(luxb > maxLX /*&& (cnt == 1)*/) 
+      {
+        Serial.print(" Current max in Back :  ");
+        Serial.println(luxb);
+        maxLX = luxb;
+        degreeMax = j*30;
+        flag = 1;
+        max_in_front = false;
+        Serial.print("maxLX : ");
+        Serial.println(maxLX);
+        
+      }
+      if(luxf > maxLX /*&& (cnt == 0)*/) 
+      {
+        Serial.print(" Current max in Front :  ");
+        Serial.print(luxf);
+        maxLX = luxf;
+        degreeMax = j*30;
+        flag = 0;
+        max_in_front = true;
+        Serial.print("maxLX : ");
+        Serial.println(maxLX);
+        
+      }
+      delay(1000);
+   }
+   if ( flag == 1 ){
+    degreeMax = degreeMax + 180;
+   }
+ // }
+ }
+
 void setup()   { 
-
-  analogWrite(ENA, 255);
-  analogWrite(ENB, 255); 
-
-   
+  /* We connected ENA and ENB to VCC*/
+  //analogWrite(ENA, 255);   
+  //analogWrite(ENB, 255); 
 
   pinMode(LeftMotorIN1, OUTPUT);      // Defines this pin as an output. The Arduino will write values to this pin.
   pinMode(LeftMotorIN2, OUTPUT);      // Defines this pin as an output. The Arduino will write values to this pin.
   pinMode(RightMotorIN3, OUTPUT);     // Defines this pin as an output. The Arduino will write values to this pin.
   pinMode(RightMotorIN4, OUTPUT);     // Defines this pin as an output. The Arduino will write values to this pin.
-  
-                 
-  //for bh1750
+        
+  //BH1750 setup
   pinMode(FrontSensor, OUTPUT);   // gy-front
   pinMode(BackSensor, OUTPUT);   // gy-back
  
@@ -120,72 +177,23 @@ void setup()   {
   Servo1.attach(servoPin1);
   Servo2.attach(ServoPin2);
    
-  pinMode(FrontSensor, INPUT);      // Defines this pin as an input.  The Arduino will read values from this pin.
-  pinMode(BackSensor, INPUT);     // Defines this pin as an input.  The Arduino will read values from this pin.
-  
   Serial.begin(9600);              // Enables a serial connection through the Arduino to either USB or UART (pins 0&1). Note that the baud rate is set to 9600
   Serial.println(" \nBeginning Light Seeking Behavior");   // Placed at the very end of void Setup() so that it is runs once, right before the void Loop()
   
   Serial.println(F("BH1750 Test begin"));
-
   // Initialize the I2C bus (BH1750 library doesn't do this automatically)
    Wire.begin();
   // On esp8266 you can select SCL and SDA pins using Wire.begin(D4, D3);
   //Wire.begin(D4, D3);
   //Uno, Ethernet  A4 (SDA), A5 (SCL)
-
   lightMeter.begin();
- 
-
 }
-
-// the loop() method runs over and over again,
-// as long as the Arduino has power
-
 void loop()    {
-  /*
-    maxLX = 0 ; 
-   for(int j = 0 ; j < 10 ; j++)
-   {
-      Servo1.write(j*18);
-      delay(100);
-      
-      digitalWrite(FrontSensor, LOW);       // sets the digital_out_3 on -> front
-      digitalWrite(BackSensor, HIGH);       // sets the digital_out_5 off -> back
-       uint16_t luxb = lightMeter.readLightLevel();
-       delay(200); 
-         
-      digitalWrite(FrontSensor, HIGH);       // sets the digital_out_3 on -> front
-      digitalWrite(BackSensor, LOW);       // sets the digital_out_5 off -> back
-      uint16_t luxf = lightMeter.readLightLevel();
-      Serial.print("Light: ");
-      Serial.print(luxf);
-      Serial.println(" lx");
-      Serial.print(luxb);
-      Serial.println(" lx");
-      Serial.println("now change...............");
-      if(luxb > maxLX) 
-      {
-        maxLX = luxb;
-        degreeMax = j*18;
-        flag = 1;
-        
-      }
-      if(luxf > maxLX) 
-      {
-        maxLX = luxf;
-        degreeMax = j*18;
-        flag = 0;
-        
-      }
-      delay(1000);
-   }
-   if ( flag == 1 ) 
-   {
-    degreeMax = degreeMax + 180;
-   }
-  // This section of the sketch is what actually interperets the data and then runs the motors accordingly.
-*/
+
+   calc_degree();
+
+////// cm to test servo
+/*
  // if (degreeMax >= 0 &&  degreeMax < 90 ) {    
 
     move_left();
@@ -210,6 +218,6 @@ void loop()    {
     move_backward();
      delay(2000);
  // }
-  
+ */ 
 }
 
