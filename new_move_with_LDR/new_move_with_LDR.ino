298ced9f1a92e86ee8f91203b7540be8e383ae7a
Negar Mirgati <negar.nbj96@yahoo.com>
2018-06-27 13:00:05 +0430

    Pinout updated

--- new_move_with_LDR/new_move_with_LDR.ino
+++ new_move_with_LDR/new_move_with_LDR.ino
@@ -1,9 +1,6 @@
 #include <Wire.h>
 #include <Servo.h> 
 
-int i = 0;
-int flag; // 0 -> front , 1 -> left
-
 // Create  servo objects
 Servo Servo1;
 Servo Servo2; 
@@ -21,7 +18,7 @@ int trigPin2 = 13;
 
 // Declare Motor Pins
 const int RightMotorIN3 = 5;    // This pin is used to enable or disable the Right motor. Connected to the base of an NPN transistor.
-const int RightMotorIN4 = 4;    // This pin is used to enable or disable the Right motor. Connected to the base of an NPN transistor.
+const int RightMotorIN4 = 3;    // This pin is used to enable or disable the Right motor. Connected to the base of an NPN transistor.
 const int LeftMotorIN1 = 7;     // This pin is used to enable or disable the Left motor. Connected to the base of an NPN transistor.
 const int LeftMotorIN2 = 6;     // This pin is used to enable or disable the Left motor. Connected to the base of an NPN transistor.
 
@@ -48,12 +45,10 @@ int SensorDifference;   // This value is used to determine the difference betwee
 
  void move_right()  {
 
-    digitalWrite(RightMotorIN3, HIGH);                              // This is used to turn Left. Notice the 
-    digitalWrite(RightMotorIN4, LOW); 
-    
-    digitalWrite(LeftMotorIN1, LOW);                                // opposite motor runs to turn Left.
-    digitalWrite(LeftMotorIN2, HIGH); 
-    
+   digitalWrite(RightMotorIN3, HIGH);                              // This is used to turn Left. Notice the 
+   digitalWrite(RightMotorIN4, LOW);    
+   digitalWrite(LeftMotorIN1, LOW);                                // opposite motor runs to turn Left.
+   digitalWrite(LeftMotorIN2, HIGH);   
     
     Serial.println("Right");                                      // This prints Left when the robot would actually turn Left.
  }
@@ -73,7 +68,7 @@ int SensorDifference;   // This value is used to determine the difference betwee
   
     digitalWrite(RightMotorIN3, LOW);                              // This is used to turn Left. Notice the 
     digitalWrite(RightMotorIN4, HIGH); 
-    
+
     digitalWrite(LeftMotorIN1, LOW);                                // opposite motor runs to turn Left.
     digitalWrite(LeftMotorIN2, HIGH); 
     
@@ -86,10 +81,9 @@ void calc_degree(){
    Servo2.write(0);
    maxLX = 0 ; 
    degreeMax = 0;
-   //for(int cnt = 0 ; cnt <= 1; cnt++){
+
    for(int j = 0; j <= 6; j++){
       
-      /*(cnt == 0) ? */
       Servo1.write(j*30); 
       Servo2.write(j*30);    
       delay(100);
@@ -108,34 +102,29 @@ void calc_degree(){
       Serial.print(luxb);
       Serial.println(" lx");
       Serial.println("the other sensor");
-    if(luxb > maxLX /*&& (cnt == 1)*/) 
+    if(luxb > maxLX) 
       {
         Serial.print(" Current max in Back :  ");
         Serial.println(luxb);
         maxLX = luxb;
         degreeMax = j*30;
-        flag = 1;
         max_in_front = false;
         Serial.print("maxLX : ");
         Serial.println(maxLX);
         
       }
-      if(luxf > maxLX /*&& (cnt == 0)*/) 
+      if(luxf > maxLX) 
       {
         Serial.print(" Current max in Front :  ");
         Serial.print(luxf);
         maxLX = luxf;
         degreeMax = j*30;
-        flag = 0;
         max_in_front = true;
         Serial.print("maxLX : ");
         Serial.println(maxLX);
         
       }
       delay(1000);
-   }
-   if ( flag == 1 ){
-    degreeMax = degreeMax + 180;
    }
  }
 
@@ -166,33 +155,22 @@ void setup()   {
 void loop()    {
 
    calc_degree();
-
-////// cm to test servo
-/*
- // if (degreeMax >= 0 &&  degreeMax < 90 ) {    
-
+   if (degreeMax >= 0 &&  degreeMax < 90 ) {    
     move_left();
     delay(2000);
    
- // }
-
- // else if (degreeMax > 90 &&  degreeMax < 180 ) {    
- 
-    move_right();
+  }
+  else if (degreeMax > 90 &&  degreeMax < 180) {
+    if(max_in_front)
+      move_right();
     delay(2000);
- // }
-
- // else if (degreeMax == 90 ) {                               
-
-    move_forward();
-     delay(2000);
- // }
-  
- //  else if (degreeMax == 270 ) {                               
-
-    move_backward();
-     delay(2000);
- // }
- */ 
+  }
+  else if (degreeMax == 90  ) {         
+    if(max_in_front)                      
+      move_forward();
+     else
+      move_backward();
+      delay(2000);
+  } 
 }
 
